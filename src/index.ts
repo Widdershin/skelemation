bb627b410a948c9f31ba2f3ce46c41eae9ac6a76
Nick Johnstone <ncwjohnstone@gmail.com>
2017-03-24 18:38:14 +1300

    Animate all the things

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {makeDOMDriver, div, h, pre, button, DOMSource, VNode, input} from '@cycle/dom';
+import {DOMSource, VNode, makeDOMDriver, div, h, pre, button, input, h3} from '@cycle/dom';
 import {run} from '@cycle/run';
 import {timeDriver} from '@cycle/time';
 import {TimeSource} from '@cycle/time/dist/time-source';
@@ -20,122 +20,165 @@ const skellington : Bone = {
     "x": 0,
     "y": 0
   },
+  "rotationFormula": "a",
   "children": [
     {
       "vector": {
-        "x": 12.800000190734863,
-        "y": 36.79999923706055
+        "x": 0,
+        "y": -64
       },
-      "id": 1,
+      "id": 0,
       "children": [
         {
           "vector": {
-            "x": 0.8000001907348633,
-            "y": 35.20000076293945
+            "x": 20,
+            "y": 10.400001525878906
           },
-          "id": 2,
+          "id": 1,
           "children": [
             {
               "vector": {
-                "x": 15.19999885559082,
-                "y": 5.599998474121094
+                "x": 0.7999992370605469,
+                "y": 38.39999866485596
               },
-              "id": 3,
-              "children": []
-            }
-          ]
-        }
-      ]
-    },
-    {
-      "vector": {
-        "x": -7.199999809265137,
-        "y": 33.599998474121094
-      },
-      "id": 4,
-      "children": [
-        {
-          "vector": {
-            "x": -0.8000001907348633,
-            "y": 34.400001525878906
-          },
-          "id": 5,
-          "children": [
-            {
-              "vector": {
-                "x": 11.200000047683716,
-                "y": 5.599998474121094
-              },
-              "id": 6,
-              "children": []
-            }
-          ]
-        }
-      ]
-    },
-    {
-      "vector": {
-        "x": -0.800000011920929,
-        "y": -36
-      },
-      "id": 7,
-      "children": [
-        {
-          "vector": {
-            "x": -0.800000011920929,
-            "y": -31.199996948242188
-          },
-          "id": 8,
-          "children": [
-            {
-              "vector": {
-                "x": 19.99999964237213,
-                "y": 25.599998474121094
-              },
-              "id": 9,
-              "children": [
-                {
-                  "vector": {
-                    "x": 0.8000011444091797,
-                    "y": 38.39999842643738
-                  },
-                  "id": 10,
-                  "children": []
-                }
-              ]
-            },
-            {
-              "vector": {
-                "x": -16.00000035762787,
-                "y": 23.999996185302734
-              },
-              "id": 11,
+              "id": 2,
               "children": [
                 {
                   "vector": {
                     "x": 0,
-                    "y": 39.20000076293945
+                    "y": 37.59999942779541
                   },
-                  "id": 12,
-                  "children": []
+                  "id": 3,
+                  "children": [],
+                  "rotationFormula": "a"
                 }
-              ]
-            },
+              ],
+              "rotationFormula": "a"
+            }
+          ],
+          "rotationFormula": "a"
+        },
+        {
+          "vector": {
+            "x": -19.200000762939453,
+            "y": 8.799999237060547
+          },
+          "id": 4,
+          "children": [
+            {
+              "vector": {
+                "x": -0.7999992370605469,
+                "y": 37.60000038146973
+              },
+              "id": 5,
+              "children": [
+                {
+                  "vector": {
+                    "x": -1.6000003814697266,
+                    "y": 40.80000114440918
+                  },
+                  "id": 6,
+                  "children": [],
+                  "rotationFormula": "a"
+                }
+              ],
+              "rotationFormula": "a"
+            }
+          ],
+          "rotationFormula": "a"
+        },
+        {
+          "vector": {
+            "x": 0.800000011920929,
+            "y": -31.199996948242188
+          },
+          "id": 7,
+          "children": [],
+          "rotationFormula": "a"
+        }
+      ],
+      "rotationFormula": "a"
+    },
+    {
+      "vector": {
+        "x": 7.199999809265137,
+        "y": 15.199999809265137
+      },
+      "id": 8,
+      "children": [
+        {
+          "vector": {
+            "x": 0,
+            "y": 44.000000953674316
+          },
+          "id": 9,
+          "children": [
             {
               "vector": {
                 "x": 0,
-                "y": -24
+                "y": 42.39999771118164
               },
-              "id": 13,
-              "children": []
+              "id": 10,
+              "children": [
+                {
+                  "vector": {
+                    "x": 16.800000190734863,
+                    "y": 5.599998474121094
+                  },
+                  "id": 11,
+                  "children": [],
+                  "rotationFormula": "a"
+                }
+              ],
+              "rotationFormula": "a"
             }
-          ]
+          ],
+          "rotationFormula": "a"
         }
-      ]
+      ],
+      "rotationFormula": "a"
+    },
+    {
+      "vector": {
+        "x": -8.800000190734863,
+        "y": 13.600000381469727
+      },
+      "id": 12,
+      "children": [
+        {
+          "vector": {
+            "x": 0.8000001907348633,
+            "y": 43.19999885559082
+          },
+          "id": 13,
+          "children": [
+            {
+              "vector": {
+                "x": 0,
+                "y": 45.60000228881836
+              },
+              "id": 14,
+              "children": [
+                {
+                  "vector": {
+                    "x": 15.199999809265137,
+                    "y": 7.1999969482421875
+                  },
+                  "id": 15,
+                  "children": [],
+                  "rotationFormula": "a"
+                }
+              ],
+              "rotationFormula": "a"
+            }
+          ],
+          "rotationFormula": "a"
+        }
+      ],
+      "rotationFormula": "a"
     }
   ]
 }
-
 type Sources = {
   DOM: DOMSource,
   Time: TimeSource
@@ -162,10 +205,28 @@ type Bone = {
   id: number;
   vector: Vector;
   children: Bone[];
+  rotationFormula: string;
 }
 
-function renderBone (bone: Bone, parentPosition: Vector, address: Address): VNode {
-  const position = add(parentPosition, bone.vector);
+function rotate (point: Vector, angle: number) {
+  return {
+    x: point.x * Math.cos(angle) - point.y * Math.sin(angle),
+    y: point.x * Math.sin(angle) + point.y * Math.cos(angle)
+  }
+}
+
+function renderBone (bone: Bone, parentPosition: Vector, address: Address, waveRotation: number, parentRotation: number): VNode {
+  const boneAngle = angle(bone.vector);
+  let rotationToApply = parentRotation;
+
+  if (bone.rotationFormula !== 'a') {
+    const s = waveRotation;
+    const a = boneAngle;
+
+    rotationToApply += eval(bone.rotationFormula) - boneAngle;
+  }
+
+  const position = add(parentPosition, rotate(bone.vector, rotationToApply));
 
   return (
     h('g', [
@@ -203,14 +264,24 @@ function flatten (arr: any): any {
 }
 
 
-function renderSkeleton (bone: Bone, parentPosition: Vector, parentAddress: Address): VNode[] {
-  const position = add(parentPosition, bone.vector);
+function renderSkeleton (bone: Bone, parentPosition: Vector, parentAddress: Address, waveRotation: number, parentRotation: number): VNode[] {
+  const boneAngle = angle(bone.vector);
+  let rotationToApply = parentRotation;
+
+  if (bone.rotationFormula !== 'a') {
+    const s = waveRotation;
+    const a = boneAngle;
+
+    rotationToApply += eval(bone.rotationFormula) - boneAngle;
+  }
+
+  const position = add(parentPosition, rotate(bone.vector, rotationToApply));
   const address = parentAddress.concat(bone.id);
 
   return [
-    renderBone(bone, parentPosition, address),
+    renderBone(bone, parentPosition, address, waveRotation, parentRotation),
 
-    ...flatten(bone.children.map(childBone => renderSkeleton(childBone, position, address)))
+    ...flatten(bone.children.map(childBone => renderSkeleton(childBone, position, address, waveRotation, rotationToApply)))
   ];
 }
 
@@ -225,7 +296,9 @@ function view (state: State) {
   const renderedBonesTree = renderSkeleton(
     state.rootBone,
     {x: 0, y: 0},
-    []
+    [],
+    0,
+    0
   );
 
   return (
@@ -271,7 +344,7 @@ function addBone (bone: Bone, address: Address, newBone: {vector: Vector}): Bone
   const id = idMaker();
 
   const boneToAddTo = findBone(bone, address);
-  boneToAddTo.children.push({...newBone, id, children: []});
+  boneToAddTo.children.push({...newBone, id, children: [], rotationFormula: 'a'});
 
   return bone;
 }
@@ -312,7 +385,7 @@ function bonesOnAddress (bone: Bone, address: Address): Bone[] {
 }
 
 function findBone (bone: Bone, address: Address): Bone {
-  const fauxRootBone = {children: [bone], vector: {x: 0, y: 0}, id: -1};
+  const fauxRootBone = {children: [bone], vector: {x: 0, y: 0}, id: -1, rotationFormula: 'a'};
 
   return address.reduce((bone, id) => {
     const boneIndex = bone.children.findIndex((bone: Bone) => bone.id === id);
@@ -432,24 +505,25 @@ type AnimatorState = {
   skeleton: Bone,
   waveProgress: number,
   waveLength: number,
-  waveFormula: string
+  waveFormula: string,
+  selectedBoneAddress: null | Address
 }
 
+function yAtPoint (formula: string, x: number): number  {
+  const code = `
+    var x = ${x};
+
+    ${formula};
+  `;
+
+  return -eval(code);
+};
+
 function renderWavePath (state: AnimatorState, width: number, pixels: number): VNode {
-  const yAtPoint = (x: number) => {
-    const code = `
-      var x = ${x};
-
-      ${state.waveFormula};
-    `;
-
-    return eval(code);
-  };
-
   const points = new Array(pixels).fill(0).map((_, index) => {
     const x = index / pixels * width;
 
-    return `L ${x} ${yAtPoint(x)}`;
+    return `L ${x} ${yAtPoint(state.waveFormula, x)}`;
   });
 
   return (
@@ -462,6 +536,27 @@ function renderWavePath (state: AnimatorState, width: number, pixels: number): V
       }
     })
   )
+}
+
+function renderRotationFormulaControls (state: AnimatorState): VNode {
+  const bone = findBone(state.skeleton, state.selectedBoneAddress as Address);
+  return (
+    div('.rotation-formula-controls', [
+      div('.title', `Rotation formula for ${state.selectedBoneAddress}`),
+      input('.rotation-formula', {props: {value: bone.rotationFormula}}),
+      pre(JSON.stringify(bone, null, 2))
+    ])
+  );
+}
+
+function updateBone (rootBone: Bone, address: Address, update: Object): Bone {
+  const bone = findBone(rootBone, address);
+
+  Object.keys(update).forEach(key => {
+    (bone as any)[key] = (update as any)[key];
+  });
+
+  return rootBone;
 }
 
 function SkeletonAnimator (sources: Sources): Sinks {
@@ -483,9 +578,30 @@ function SkeletonAnimator (sources: Sources): Sinks {
     .map(ev => (ev.target as HTMLInputElement).value)
     .map((formula: string): Reducer<AnimatorState> => (state) => ({...state, waveFormula: formula}));
 
+  const selectBone$ = sources.DOM
+    .select('circle')
+    .events('click')
+    .map((event: any): Reducer<AnimatorState> => {
+      return function (state: AnimatorState): AnimatorState {
+        const address = (event.currentTarget as Element).getAttribute('boneAddress');
+
+        state.selectedBoneAddress = (address as string).split(',').map(s => parseInt(s, 10));
+
+        return state;
+      }
+    });
+
+  const updateBoneRotationFormula$ = sources.DOM
+    .select('.rotation-formula')
+    .events('change')
+    .map(ev => (ev.target as HTMLInputElement).value)
+    .map((formula: string): Reducer<AnimatorState> => (state) => ({...state, skeleton: updateBone(state.skeleton, state.selectedBoneAddress as Address, {rotationFormula: formula})}));
+
   const reducer$ = xs.merge(
     update$,
-    changeWaveform$
+    changeWaveform$,
+    selectBone$,
+    updateBoneRotationFormula$
   );
 
   const state$ = reducer$.fold(applyReducer, initialState);
@@ -494,13 +610,15 @@ function SkeletonAnimator (sources: Sources): Sinks {
     const viewBox = `-320 -240 640 480`;
     const waveViewBox = `0 -0.5 ${TAU} 1`;
 
+    const waveProgress = state.waveProgress / state.waveLength % 1;
+
     const renderedBonesTree = renderSkeleton(
       state.skeleton,
       {x: 0, y: 0},
-      []
+      [],
+      yAtPoint(state.waveFormula, TAU * waveProgress),
+      0
     );
-
-    const waveProgress = state.waveProgress / state.waveLength % 1;
 
     return (
       div('.animator', [
@@ -509,6 +627,7 @@ function SkeletonAnimator (sources: Sources): Sinks {
         ]),
 
         div('.waves', [
+          h3('s'),
           h('svg', {attrs: {viewBox: waveViewBox, width: 300, height: 200}}, [
             h('line', {
               attrs: {
@@ -532,12 +651,16 @@ function SkeletonAnimator (sources: Sources): Sinks {
                 stroke: 'darkgrey',
                 'stroke-width': 0.02
               }
-            })
+            }),
           ]),
 
           div('.wave-control', [
             `y = `,
             input('.wave-formula', {attrs: {value: state.waveFormula}})
+          ]),
+
+          div('.selected-bone', [
+            state.selectedBoneAddress ? renderRotationFormulaControls(state) : ''
           ])
         ])
       ])
@@ -547,6 +670,10 @@ function SkeletonAnimator (sources: Sources): Sinks {
   return {
     DOM: state$.map(view)
   }
+}
+
+function angle (vector: Vector): number {
+  return Math.atan2(-vector.y, vector.x);
 }
 
 type MainState = {
